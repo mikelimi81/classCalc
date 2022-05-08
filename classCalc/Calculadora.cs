65e151e3d77cd6a56237df15ddc933a01e2d3347
Mikel <Mikel@DESKTOP-6T41FB2>
2022-05-08 17:18:40 +0200

    Agregar archivos de proyecto.

---
+++ classCalc/Calculadora.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Calculadora
+{
+    public int Suma(int num1, int num2)
+    {
+        return num1 + num2;
+    } 
+
+    public int Resta(int num1, int num2)
+    {
+        return num1 - num2;
+    } 
+
+    public int Mult(int num1, int num2)
+    {
+        return num1 * num2;
+    } 
+
+    public double Div(double num1, double num2)
+    {
+        if (num2 !=0 )
+        {
+            return num1 / num2;
+        }
+        else
+        {
+            Console.WriteLine("ERROR: No se puede dividir por 0");
+            return 0;
+        }
+    }
+    
+}
---
+++ classCalc/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+            int a = 1;
+            int b = 2;
+            
+            Console.WriteLine(a + " + " + b + " = " + calc.Suma(a,b));
+            Console.WriteLine(a + " - " + b + " = " + calc.Resta(a,b));
+            Console.WriteLine(a + " * " + b + " = " + calc.Mult(a,b));
+            Console.WriteLine(a + " / " + b + " = " + calc.Div(a,b));
+
+            Console.Read();
+        }
+    }
+}
